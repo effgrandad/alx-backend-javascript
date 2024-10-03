c69744f20bbd347f21c73e1a186471cba8d96445
effgrandad <effgrandad@gmail.com>
2024-10-03 19:10:29 +0000

    commit es6 classes

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,15 @@
+/**
+ * dipiction of a class room
+ */
+export default class ClassRoom
+{
+	/**
+	 * make a new @see {@link ClassRoom}.
+	 * @para {Number} maxStudentsSize - The highest quantity of
+	 * pupils allowed in the course
+	 */
+	constructor(maxStudentsSize)
+	{
+		this._maxStudentsSize = maxStudentsSize;
+	}
+}
