be44845c9f284a2ac94f9d3ae83261900a305390
effgrandad <effgrandad@gmail.com>
2024-10-05 00:55:11 +0000

    commit corrections

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,14 +1,5 @@
 #!/usr/bin/env node
 
-//eslint-disable no-underscore-dangle
-
-/**
- * dipiction of a class room
- *
- * make a new @see {@link ClassRoom}.
- * @para {Number} maxStudentsSize - The highest quantity of
- * pupils allowed in the course
- */
 export default class ClassRoom {
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
