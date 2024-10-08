93ac5cac9faee65ac0e7d8ff85155f3d057282b7
effgrandad <effgrandad@gmail.com>
2024-10-08 15:29:29 +0000

    advance task

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,10 +1,12 @@
 export const weakMap = new WeakMap();
 
-export function queryAPI(endPoint) {
-  const total = weakMap.get(endPoint) || 0;
-  weakMap.set(endPoint, total + 1);
-  if (total >= 5) {
+const MAX_ENDPOINT_CALLS = 5;
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
     throw new Error('Endpoint load is high');
   }
-  return total;
 }
